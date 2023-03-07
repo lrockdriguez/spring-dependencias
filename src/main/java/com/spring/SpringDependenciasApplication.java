340a5b2bd02f7f6b93eeac82c264d23c15abb2b4
lrockdriguez <luisfina@hotmail.com>
2023-03-07 17:32:50 -0600

    mi primer commit

---
+++ src/main/java/com/spring/SpringDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependenciasApplication.class, args);
+	}
+
+}
